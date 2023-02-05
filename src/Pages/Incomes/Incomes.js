d3e00e73be4c5ce67a80d91a035e4b97eae10691
Adi Mansbach <adi.mansbach@ourcrowd.com>
2023-02-05 21:46:23 +0200

    changw #2 after integration

--- src/Pages/Incomes/Incomes.js
+++ src/Pages/Incomes/Incomes.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import IncomesTable from '../../Components/IncomesTable/IncomesTable';
