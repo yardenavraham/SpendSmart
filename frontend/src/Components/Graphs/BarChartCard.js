44f18a990904cfd30b74d6cbf394d909e4c80b54
Yaara Rumney <yaararm@gmail.com>
2023-02-18 10:23:23 +0200

    add select month button and colors to graph

--- frontend/src/Components/Graphs/BarChartCard.js
+++ frontend/src/Components/Graphs/BarChartCard.js
@@ -1,35 +1,52 @@
-import React from 'react';
-import {Typography, Grid, Card, CardContent} from "@mui/material";
-import {BarChart, CartesianGrid, YAxis, XAxis, Tooltip, Legend, Bar, ResponsiveContainer} from "recharts";
-import {ThemeProvider} from '@mui/material/styles';
-import {useTheme} from "@mui/styles";
+import React from "react";
+import { Typography, Grid, Card, CardContent } from "@mui/material";
+import {
+  BarChart,
+  CartesianGrid,
+  YAxis,
+  XAxis,
+  Tooltip,
+  Legend,
+  Bar,
+  Cell,
+  ResponsiveContainer,
+} from "recharts";
+import { ThemeProvider } from "@mui/material/styles";
+import { useTheme } from "@mui/styles";
+import * as d3 from 'd3';
 
 function BarChartCard(props) {
-    const theme = useTheme();
+  const theme = useTheme();
+  const colors = d3.schemeCategory10;
 
-    return (
-        <Grid item xs={12}>
-            <Card elevation="0">
-                <CardContent>
-                    <ThemeProvider theme={theme}>
-
-                        <Typography variant="h3">{props.header}</Typography>
-                    </ThemeProvider>
-                    <ResponsiveContainer height={400}>
-                        <BarChart width={500} height={400} data={props.data}>
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis dataKey="value"/>
-                            <Tooltip />
-                            {/*<Legend />*/}
-                            <Bar dataKey="value" fill={theme.palette.primary.main} />
-                        </BarChart>
-                    </ResponsiveContainer>
-                </CardContent>
-            </Card>
-        </Grid>
-
-    );
+  return (
+    <Grid item xs={12}>
+      <Card elevation="0">
+        <CardContent>
+          <ThemeProvider theme={theme}>
+            <Typography variant="h3">{props.header}</Typography>
+          </ThemeProvider>
+          <ResponsiveContainer height={400}>
+            <BarChart width={500} height={400} data={props.data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis dataKey="value" />
+              <Tooltip />
+              {/*<Legend />*/}
+              <Bar dataKey="value" fill={theme.palette.primary.main}>
+                {props.data.map((entry, index) => (
+                  <Cell
+                    key={`cell-${index}`}
+                    fill={colors[index % colors.length]}
+                  />
+                ))}
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 }
 
 export default BarChartCard;
--- frontend/src/Components/Graphs/PieChartCard.js
+++ frontend/src/Components/Graphs/PieChartCard.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import {Card, CardContent, Grid, Typography} from "@mui/material";
-import {PieChart, Pie, Tooltip, ResponsiveContainer} from "recharts"
-import {ThemeProvider} from '@mui/material/styles';
-import {useTheme} from "@mui/styles";
+import React from "react";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
+import { PieChart, Pie,  Cell, Tooltip, ResponsiveContainer } from "recharts";
+import { ThemeProvider } from "@mui/material/styles";
+import { useTheme } from "@mui/styles";
+import * as d3 from 'd3';
 
 function PieChartCard(props) {
-    const theme = useTheme();
+  const theme = useTheme();
+  const colors = d3.schemeCategory10;
 
-    return (
-        <Grid item xs={4}>
-            <Card elevation="0">
-                <CardContent>
-                    <ThemeProvider theme={props.theme}>
-                        <Typography variant="h6">{props.header}</Typography>
-                    </ThemeProvider>
-                    <ResponsiveContainer height={200}>
-
-                        <PieChart width={150} height={200}>
-                            <Pie
-                                data={props.data}
-                                dataKey="value"
-                                nameKey={"name"}
-                                cx="50%"
-                                cy="50%"
-                                outerRadius="100%"
-                                fill={theme.palette.primary.main}
-                                legendType="diamond"
-                            />
-                            <Tooltip/>
-                        </PieChart>
-                    </ResponsiveContainer>
-                </CardContent>
-            </Card>
-        </Grid>
-    );
+  return (
+    <Grid item xs={4}>
+      <Card elevation="0">
+        <CardContent>
+          <ThemeProvider theme={props.theme}>
+            <Typography variant="h6">{props.header}</Typography>
+          </ThemeProvider>
+          <ResponsiveContainer height={200}>
+            <PieChart width={150} height={200}>
+              <Pie
+                data={props.data}
+                dataKey="value"
+                nameKey={"name"}
+                cx="50%"
+                cy="50%"
+                outerRadius="100%"
+                fill={theme.palette.primary.main}
+                legendType="diamond"
+              >
+            {props.data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+            ))}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
 }
 
 export default PieChartCard;
