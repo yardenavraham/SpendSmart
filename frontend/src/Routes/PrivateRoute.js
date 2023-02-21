2aa657106677476f56fd6485ada548f1d854413a
Adi Mansbach <adi.mansbach@ourcrowd.com>
2023-02-21 13:23:23 +0200

    fix refresh issue

--- frontend/src/Routes/PrivateRoute.js
+++ frontend/src/Routes/PrivateRoute.js
@@ -6,6 +6,8 @@ const PrivateRoute = () => {
     const authCtx = useContext(AuthContext);
     // If authorized, return an outlet that will render child elements
     // If not, return element that will navigate to login page
-    return authCtx.isLoggedIn ? <Outlet /> : <Navigate to="/signin" />;
+    console.log('authCtx.isLoggedIn11 ' + authCtx.isLoggedIn);
+    //return authCtx.isLoggedIn ? <Outlet /> : <Navigate to="/signin" />;
+    return localStorage.getItem('isLoggedIn') ? <Outlet /> : <Navigate to="/signin" />;
 }
 export default PrivateRoute;
