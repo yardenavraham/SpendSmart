bdf7bd1fb2ff2daea2b1d4009f02bd88c8533f92
Adi Mansbach <adi.mansbach@ourcrowd.com>
2023-03-11 23:24:03 +0200

    save change until now

--- frontend/src/Components/UploadImage/UploadImage.js
+++ frontend/src/Components/UploadImage/UploadImage.js
@@ -24,78 +24,90 @@ const UploadImage = (props) => {
     console.log('response', response.data.file);
     setFile(response.data.file);
 
-}
+  }
 
-const headd = 'http://localhost:27017/';
-const path = 'uploads/Screen Shot 2023-03-03 at 0.34.14.png-1678441002948';
-const src1 = headd+path;
-console.log('src1', src1);
+  const removeClicked = (event) => {
+    event.preventDefault();
+    console.log('removeClicked');
+    console.log('selectedImage', selectedImage);
+    if (selectedImage === undefined) {
+      setPreview(selectedImage);
+    }
+    else {
+      setSelectedImage(null);
+    }
+  }
 
-const getUrlExtension = (url) => {
-  return url
-    .split(/[#?]/)[0]
-    .split(".")
-    .pop()
-    .trim();
-}
+  const headd = 'http://localhost:27017/';
+  const path = 'uploads/Screen Shot 2022-10-04 at 10.43.43.png-1678451568386';
+  let src1 = headd+path;
+  console.log('src1', src1);
 
-const onImageEdit = async (imgUrl) => {
-  console.log('onImageEdit');
-  console.log('imgUrl', imgUrl);
+// const getUrlExtension = (url) => {
+//   return url
+//     .split(/[#?]/)[0]
+//     .split(".")
+//     .pop()
+//     .trim();
+// }
 
-  var imgExt = getUrlExtension(imgUrl);
-  console.log('imgExt', imgExt);
+// const onImageEdit = async (imgUrl) => {
+//   console.log('onImageEdit');
+//   console.log('imgUrl', imgUrl);
 
-  const response = await fetch(imgUrl);
-  const blob = await response.blob();
-  const file = new File([blob], "profileImage." + imgExt, {
-    type: blob.type,
-  });
-  console.log('file222', file);
-  console.log('type file', typeof file);
+//   var imgExt = getUrlExtension(imgUrl);
+//   console.log('imgExt', imgExt);
 
-  return file;
-}
+//   const response = await fetch(imgUrl);
+//   const blob = await response.blob();
+//   const file = new File([blob], "profileImage." + imgExt, {
+//     type: blob.type,
+//   });
+//   console.log('file222', file);
+//   console.log('type file', typeof file);
 
-const file1 = onImageEdit(src1);
-console.log('type file1', typeof file1);
+//   return file;
+// }
 
-console.log('file1', file1);
-// if (selectedImage !== undefined) console.log('URL.createObjectURL(selectedImage)', URL.createObjectURL(selectedImage));
+// const file1 = onImageEdit(src1);
+// console.log('type file1', typeof file1);
 
-const preview = selectedImage === undefined ? onImageEdit(src1) : selectedImage;
+// console.log('file1', file1);
+
+  const [preview, setPreview] = useState(selectedImage === undefined ? src1 : URL.createObjectURL(selectedImage));
 
   return (
     <div>
+      {/* <img src ={src1} alt="not found" /> */}
       <form action="http://localhost:27017/uploadimage" method="post" encType="multipart/form-data">
 
-      {selectedImage && (
-        <div>
-          <img
-            className='upload-image-preview'
-            alt="not found"
-            width={"250px"}
-            src={URL.createObjectURL(selectedImage)}
-            onClick={() => setSelectedImage(null)}
-          />
-          <br />
-          <button onClick={() => setSelectedImage(null)}>Remove</button>
-        </div>
-      )}
+        { (
+          <div>
+            <img
+              className='upload-image-preview'
+              alt="not found"
+              width={"250px"}
+              src={preview}
+              onClick={() => setSelectedImage(null)}
+            />
+            <br />
+            <button onClick={(event) => removeClicked(event)}>Remove</button>
+          </div>
+        )}
 
-      <br />
-      <br />
-      
-      <input
-       type="file" 
-       name="file" 
-       id="file" 
-        onChange={(event) => {
-          console.log('file', event.target.files[0].name);
-          setSelectedImage(event.target.files[0]);
-        }}
-      />
-      <br /><button className="submitBtn" type="submit" onClick={fileUploadHandler}>Add File</button>
+        <br />
+        <br />
+        
+        <input
+        type="file" 
+        name="file" 
+        id="file" 
+          onChange={(event) => {
+            console.log('file', event.target.files[0].name);
+            setSelectedImage(event.target.files[0]);
+          }}
+        />
+        <br/><button className="submitBtn" type="submit" onClick={fileUploadHandler}>Add File</button>
       </form>
     </div>
   );
